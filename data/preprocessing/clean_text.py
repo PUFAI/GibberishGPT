e1abae98f29cb200af5ec615f55301d5f935fb98
Michaelgathara <mikegtrm@gmail.com>
2025-03-17 03:38:35 -0500

    finalized tensor pt2

--- data/preprocessing/clean_text.py
+++ data/preprocessing/clean_text.py
@@ -1,12 +1,23 @@
 import re
+import unicodedata
+
+pattern_equals = re.compile(r'={2,}')  # repeated equal signs 
+pattern_at = re.compile(r'@-@')        
+pattern_space_before_punct = re.compile(r'\s+([.,!?])')  # extra space before punctuation
+pattern_space_before_s = re.compile(r"\s+'s")  # space before "'s"
+pattern_multi_space = re.compile(r'\s+')
+
 def clean_textdata(text):
-    text = re.sub(r'={2,}', '', text)  
+    text = unicodedata.normalize("NFKC", text)
     
-    text = re.sub(r'@-@', '', text)
+    text = pattern_equals.sub('', text)
+    text = pattern_at.sub('', text)
     
-    text = re.sub(r'\s+([.,])', r'\1', text)
-    text = re.sub(r'\s+\'s', r"'s", text)
+    # extra spaces before punctuation and contractions and stuff 
+    text = pattern_space_before_punct.sub(r'\1', text)
+    text = pattern_space_before_s.sub(r"'s", text)
     
-    text = re.sub(r'\s+', ' ', text).strip()
+    # normalizing whitespace and trimming stuff
+    text = pattern_multi_space.sub(' ', text).strip()
     
     return text
