9e4473ceae1e0219f62f0c37f95bec3e669bcdfd
michaelgathara <mikegtrm@gmail.com>
2025-03-20 02:25:25 -0500

    smaller training

--- models/gpt_train_script.py
+++ models/gpt_train_script.py
@@ -35,17 +35,17 @@ from tokenization import get_tiktoken_tokenizer
 print(f"Using device: {device}")
 
 
-batch_size = 64  # Kept the same; could be adjusted based on hardware
-block_size = 1024  # GPT-2 uses a context length of 1024 tokens
+batch_size = 16  # Kept the same; could be adjusted based on hardware
+block_size = 512  # GPT-2 uses a context length of 1024 tokens
 max_iters = 1  # More iterations needed for larger models
 eval_interval = 1  # Increase since more iterations are done
-learning_rate = 5e-5  # GPT-2 uses a lower learning rate
+learning_rate = 2.5e-5  # GPT-2 uses a lower learning rate
 eval_iters = 1  # More frequent evaluation for stability
 
-n_embd = 768  # GPT-2 uses 768 for the small version, 1024 for medium, 1280 for large, 1600 for XL
-n_head = 12  # GPT-2 uses 12 attention heads
-n_layer = 12  # GPT-2 has 12 transformer blocks in the small version
-dropout = 0.1  # GPT-2 uses 0.1 dropout for better generalization
+n_embd = 384  # GPT-2 uses 768 for the small version, 1024 for medium, 1280 for large, 1600 for XL
+n_head = 6 # GPT-2 uses 12 attention heads
+n_layer = 6  # GPT-2 has 12 transformer blocks in the small version
+dropout = 0.05  # GPT-2 uses 0.1 dropout for better generalization
 
 
 accumulation_steps = 4
