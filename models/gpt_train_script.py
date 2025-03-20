04e9d9539c574ec5e59c5b7e0c48770a87f3cbea
Michaelgathara <mikegtrm@gmail.com>
2025-03-20 14:12:58 -0500

    hyperparam

--- models/gpt_train_script.py
+++ models/gpt_train_script.py
@@ -35,14 +35,14 @@ from tokenization import get_tiktoken_tokenizer
 print(f"Using device: {device}")
 
 
-batch_size = 128  # Kept the same; could be adjusted based on hardware
+batch_size = 64  # Kept the same; could be adjusted based on hardware
 block_size = 512  # GPT-2 uses a context length of 1024 tokens
-max_iters = 10000  # More iterations needed for larger models
+max_iters = 1000  # More iterations needed for larger models
 eval_interval = 100  # Increase since more iterations are done
 learning_rate = 3e-4  # GPT-2 uses a lower learning rate
 eval_iters = 5  # More frequent evaluation for stability
 
-n_embd = 768  # GPT-2 uses 768 for the small version, 1024 for medium, 1280 for large, 1600 for XL
+n_embd = 1024  # GPT-2 uses 768 for the small version, 1024 for medium, 1280 for large, 1600 for XL
 n_head = 12 # GPT-2 uses 12 attention heads
 n_layer = 12  # GPT-2 has 12 transformer blocks in the small version
 dropout = 0.1  # GPT-2 uses 0.1 dropout for better generalization
@@ -477,12 +477,7 @@ class TransformerModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
 
-
- 
-# # Training
-
-
-
+# Training
 vocab_size = tokenizer.n_vocab
 
 model = TransformerModel(
