261f1c6648bdaef437c1772f969ce6a16a338330
michaelgathara <mikegtrm@gmail.com>
2025-03-22 00:35:46 -0500

    more

--- models/gpt_a100.py
+++ models/gpt_a100.py
@@ -55,8 +55,8 @@ class ModelConfig:
         self.batch_size = 64                # Batch size per GPU
         self.block_size = 512               # Context size
         self.n_embd = 512                  # Embedding dimension
-        self.n_head = 8                    # Number of attention heads
-        self.n_layer = 4                   # Number of transformer layers
+        self.n_head = 12                    # Number of attention heads
+        self.n_layer = 8                   # Number of transformer layers
         self.dropout = 0.1                  # Dropout rate
         
         # training parameters
